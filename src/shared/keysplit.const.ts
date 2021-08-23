b11493498a005390ad694ea875be6d4f92e8f81d
Danh, Huynh Thanh <danh@spiraledge.com>
2021-08-23 21:46:56 +0700

    change https

--- src/shared/keysplit.const.ts
+++ src/shared/keysplit.const.ts
@@ -1,6 +1,6 @@
 export const KeySplit = {
     TTAD: {
-        Host: 'http://truyentranhaudio.online/',
+        Host: 'https://truyentranhaudio.online/',
         KeyImg: 'div.chapter-content img._lazy.chapter-img',
         KeyChapter: 'ul.list-chapters li',
     }
